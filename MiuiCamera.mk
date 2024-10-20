0b5a0dafd41f62f6212364a27efaae78f6882c15
clarencelol <clarencekuiek@proton.me>
2024-10-20 22:32:54 +0800

    miuicamera: Re-organize miuicamera
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- MiuiCamera.mk
+++ MiuiCamera.mk
@@ -4,27 +4,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Prebuilt APKs
-PRODUCT_PACKAGES += \
-    MiuiCamera \
-    MiuiExtraPhoto
+# Build rules
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
+# Dexpreopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     MiuiCamera \
     MiuiExtraPhoto
 
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/prebuilt/MiuiCamera/lib,$(TARGET_COPY_OUT_SYSTEM_EXT)/priv-app/MiuiCamera/lib) \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/prebuilt/MiuiExtraPhoto/lib,$(TARGET_COPY_OUT_SYSTEM_EXT)/app/MiuiExtraPhoto/lib)
-
 # Properties
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    persist.vendor.camera.privapp.list=com.android.camera \
-    ro.com.google.lens.oem_camera_package=com.android.camera \
-    ro.miui.build.region=in \
-    ro.miui.notch=1 \
-    ro.product.mod_device=_global \
-    vendor.camera.aux.packagelist=com.android.camera
+TARGET_SYSTEM_EXT_PROP += vendor/xiaomi/miuicamera/system_ext.prop
+
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += vendor/xiaomi/miuicamera/sepolicy/vendor
 
 # Shims
 PRODUCT_PACKAGES += \
@@ -33,3 +26,6 @@ PRODUCT_PACKAGES += \
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/miuicamera
+
+# Inherit proprietary targets
+$(call inherit-product, vendor/xiaomi/miuicamera/common/common-vendor.mk)
